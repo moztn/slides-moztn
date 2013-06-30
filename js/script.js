c7136838fc9cfcc008e38c36dcb465795d903e71
medfiras <hajjejfiras@gmail.com>
2013-06-30 07:45:11 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  // Lowering the opacity of all slide in divs
+	$('.banner div').css('opacity',0.4);
+
+	// Using the hover method 
+	$('.banner').hover(function(){
+
+		// Executed on mouseenter
+		
+		var el = $(this);
+		
+		// Find all the divs inside the banner div,
+		// and animate them with the new size
+		
+		el.find('div').stop().animate({width:280,height:200},'slow',function(){
+			// Show the "Visit Company" text:
+			el.find('p').fadeIn('fast');
+		});
+
+	},function(){
+
+		// Executed on moseleave
+
+		var el = $(this);
+		
+		// Hiding the text
+		el.find('p').stop(true,true).hide();
+		
+		// Animating the divs
+		el.find('div').stop().animate({width:60,height:60},'slow');
+
+	}).click(function(){
+		
+		// When clicked, open a tab with the address of the hyperlink
+		
+		//window.open($(this).find('a').attr('href'));
+		
+	});
+});
