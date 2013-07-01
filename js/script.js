3581de9341344a00c336f340f74a66c7fb160828
medfiras <hajjejfiras@gmail.com>
2013-07-01 04:56:44 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   // Lowering the opacity of all slide in divs
-	$('.banner div').css('opacity',0.4);
+	$('.banner div').css('opacity',0.6);
 
 	// Using the hover method 
 	$('.banner').hover(function(){
@@ -13,7 +13,7 @@ $(document).ready(function(){
 		// Find all the divs inside the banner div,
 		// and animate them with the new size
 		
-		el.find('div').stop().animate({width:300,height:160},'slow',function(){
+		el.find('div').stop().animate({width:300,height:300},'slow',function(){
 			// Show the "Visit Company" text:
 			el.find('p').fadeIn('fast');
 		});
