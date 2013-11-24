e95e74e9be499140b5e45949d3c2d3e71ff95829
Alexandre BM <s@rednaks.tn>
2013-11-24 11:23:25 +0100

    Flask migration - part 1

---
+++ slides.py
@@ -0,0 +1,10 @@
+from flask import Flask, url_for, render_template
+app = Flask(__name__, static_path='/')
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
