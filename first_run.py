22a683c5d03c1da1d6e0b04e4518e5115a2ab4b7
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2014-10-26 21:03:06 +0100

    Adding fixtures loader.

--- first_run.py
+++ first_run.py
@@ -1,26 +1,22 @@
 import os
+
 from database import init_db
+from controllers import category_controller, slide_controller
+
 
 if __name__ == '__main__':
-  if not os.path.exists('db'):
-    os.makedirs('db')
-  init_db()
-
-
-s = Slide(
-	title="test title",
-	screenshot="img/Pres-Mozilla.png",
-	description="test desc",
-	url="http://slides.mozilla-tunisia.org/demo/Pres-Mozilla",
-	category=1
-)
-
-ss = Slide(
-	title="test title 2",
-	screenshot="img/Pres-Mozilla.png",
-	description="test desc 2",
-	url="http://moztn.github.io/firefoxOSAppDay-Slides",
-	category=1
-)
-
-
+    if not os.path.exists('db'):
+        os.makedirs('db')
+    init_db()
+    
+    print("Createing the a test category..")
+    category_controller.create(name="Test")
+    print("Creating a test slide..")
+    slide_controller.create(
+        title="test title",
+        screenshot="img/Pres-Mozilla.png",
+        description="test desc",
+        url="http://moztn.github.io/firefoxOSAppDay-Slides",
+        category=1
+    )
+    print("Fixtures created successfully")
